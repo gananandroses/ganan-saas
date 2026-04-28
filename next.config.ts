84d284e8f985cd6e0ea7941748ec7fc5b2500c20
gananandroses <ganannroses@gmail.com>
2026-04-28 08:14:50 +0300

    Disable browser cache - always load latest version

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ['192.168.10.9'],
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          { key: "Cache-Control", value: "no-cache, no-store, must-revalidate" },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
